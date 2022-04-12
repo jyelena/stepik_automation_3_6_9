b1d8b613e541f39887c6cf2789fc599578c1b460
dmikhaylov <d.y.mikhayov@yandex.ru>
2022-04-12 23:26:32 +0400

    edit assert

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ def test_should_see_add_to_basket_button(browser):
     browser.get(link)
     assert browser.find_element(
         By.CSS_SELECTOR, "button[class~='btn-add-to-basket']"
-    ), "Page has no add_to_basket button"
+    )
